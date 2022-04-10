ae348616ab4a9a3594fa2ecf99d3a586e05a8aa2
pavilandoangelo <pavilandoangelo@gmail.com>
2022-04-10 18:01:10 +0800

    change endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://nostalgias-project.herokuapp.com/posts';
+const url = 'https://nostalgias-api.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
